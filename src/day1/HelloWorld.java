d666ded7fa8910d1b661818256d89979eb0585e2
sudheer <sudheer.mca51@gmail.com>
2026-04-01 20:35:56 -0400

    Adding the code

---
+++ src/day1/HelloWorld.java
@@ -0,0 +1,30 @@
+package day1;
+
+public class HelloWorld {
+
+	public static void main(String[] args)
+	{
+//			x is a variable
+//			20 is a value
+//			int is a data type
+			
+		    int x=40;
+			System.out.println("############################Hello, World!");
+			System.out.println(x);
+			System.out.println(x);
+		 
+			
+
+	}
+	
+}
+
+/*
+{} - Curly braces
+() - Parentheses
+[] - Square brackets
+; - Semicolon
+// Single line comment
+/* Multi-line comment
+Javadoc comment
+*/
